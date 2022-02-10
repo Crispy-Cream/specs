f8fc47a55388be33d55d3bef19eb5dfdf202b930
Crispy <61794525+Crispy-Cream@users.noreply.github.com>
2022-02-10 12:22:41 -0600

    Update index.js

--- _assets/scripts/index.js
+++ _assets/scripts/index.js
@@ -1,29 +1,36 @@
-var script = document.createElement("script");
-script.src = "https://code.jquery.com/jquery-3.4.1.min.js";
-script.type = "text/javascript";
-document.getElementsByTagName("head")[0].appendChild(script);
-
 async function ChangeTheme() {
-  console.log("clicked");
-  if ($("body").hasClass("dark")) {
-    $("body").removeClass("dark");
-    $("hr").removeClass("dark");
-    $("a").removeClass("dark");
-    $("ul").removeClass("dark");
-    $("h2").removeClass("dark");
-    $("card-b").removeClass("dark");
-    $("darkmode").removeClass("dark");
-    document.getElementById("button").classList.toggle("fa-sun");
-    document.getElementById("button").classList.toggle("fa-moon");
-  } else {
-    $("body").addClass("dark");
-    $("hr").addClass("dark");
-    $("a").addClass("dark");
-    $("ul").addClass("dark");
-    $("h2").addClass("dark");
-    $("card-b").addClass("dark");
-    $("darkmode").addClass("dark");
-    document.getElementById("button").classList.toggle("fa-sun");
-    document.getElementById("button").classList.toggle("fa-moon");
+  let i = await document.documentElement.style.getPropertyValue("--main");
+  if (i === "#ffffff") {
+/*
+  --main: #16181d;
+  --txt: #e5e5fa;
+  --links: #4e78d9;
+  --button: #4e78d9;
+  --cardb: #252429;
+  --ul: #525863l
+*/
+    document.documentElement.style.setProperty("--main", "#16181d");
+    document.documentElement.style.setProperty("--txt", "#e5e5fa");
+    document.documentElement.style.setProperty("--links", "#4e78d9");
+    document.documentElement.style.setProperty("--button", "#4e78d9");
+    document.documentElement.style.setProperty("--cardb", "#252429");
+    document.documentElement.style.setProperty("--ul", "#525863");
+    console.log("Changed Theme to Dark Mode");
+  } else { 
+/*
+  --main: #ffffff;
+  --txt: #000000;
+  --links: #0000ff;
+  --button: #0000ff;
+  --cardb: #e9e9e9;
+  --ul: #000000;
+*/
+    document.documentElement.style.setProperty("--main", "#ffffff");
+    document.documentElement.style.setProperty("--txt", "#000000");
+    document.documentElement.style.setProperty("--links", "#0000ff");
+    document.documentElement.style.setProperty("--button", "#0000ff");
+    document.documentElement.style.setProperty("--cardb", "#e9e9e9");
+    document.documentElement.style.setProperty("--ul", "#000000");
+    console.log("Changed Theme to Light Mode");
   }
 }
